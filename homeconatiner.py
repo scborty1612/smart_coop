d2a439b14beee39150ddf3860ec6432bc5264ed6
Shantanu Chakraborty <scborty@gmail.com>
2017-03-07 12:04:48 +0100

    Added session based agent address retreiver.

--- homeconatiner.py
+++ homeconatiner.py
@@ -6,24 +6,77 @@ home agents.
 import aiomas
 from agents.BlockchainAgent import BlockchainAgent
 from agents.HomeAgent import HomeAgent
+from configure import Configure as CF
+
+# Database related stuff
 from sqlalchemy import create_engine
+
+import pandas as pd
+
+# System and traceback
 import sys, traceback
 
+# Logging stuffs
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Clock related stuff
+import asyncio
+import time
+import datetime
+import random
+import uuid
+
+# Agree on clocking
+CLOCK = aiomas.ExternalClock("2015-01-15T00:00:00")
+
+# Setting up the clock setting function
+async def clock_setter(factor=0.25):
+	while True:
+		await asyncio.sleep(factor)
+
+		# delta t is 15min
+		CLOCK.set_time(CLOCK.time() + (1*60*15))
+
+def createSession(agents, db_engine=None):
+	"""
+	Create a session with the home agents.
+	"""
+	session_id = uuid.uuid4().hex
+
+	# Prepare the dataframe to dump into a mysql table
+	agent_ids = []
+	agent_adds = []
+	for agent in agents:
+		agent_ids.append(agent.agent_id)
+		agent_adds.append(agent.addr)
+
+	df = pd.DataFrame(data=[agent_ids, agent_adds],)
+	df = df.T
+	df.columns = ['agent_id', 'agent_address']
+	df['session_id'] = session_id
+
+	df.to_sql(name='tbl_agents', con=db_engine, if_exists='append', index=False, chunksize=200)
+
+	return session_id
 
 def runContainer():	
+
 	# Creating container contains the home agents
 	# and prediction agents
-	HC = aiomas.Container.create(('localhost', 5556))
+	HC = aiomas.Container.create(('localhost', 5556), clock=CLOCK)
+
+	# Set the clcok
+	t_clock_setter = asyncio.async(clock_setter())
+
 
 	# List of Homes
-	homes = [9019,  7881, ]#  100237, 7850, 980, 9981,]
+	homes = [9019,  7881, 100237, 7850, 980, 9981,]
 
 	# Create the DB engine
-	db_engine = create_engine("mysql+pymysql://root@127.0.0.1/open_database")
+	# db_engine = create_engine("mysql+pymysql://{}@{}/{}".format(CF.DB_USER, CF.DB_HOST, CF.DB_NAME))
+	db_engine = CF.get_db_engine()
 
 	# Address of the blockchain agent
 	# Later, it will be retreived from the Agent Server
@@ -32,12 +85,13 @@ def runContainer():
 	# Initiate the agents into HC
 	homeAgents = [HomeAgent(container=HC, agent_id=home, db_engine=db_engine, bc_address=bc_address) for home in homes]
 
-	for homeAgent in homeAgents:
-		logger.info("{}. {}".format(homeAgent.agent_id, homeAgent.addr))
+	# Creating the session
+	session_id = createSession(agents=homeAgents, db_engine=db_engine)
 
 	# Run the event loop
 	try:
 		logger.info("Running the event loop. One of the home agents trying to connect with BC agent!")
+		logger.info("Session ID:{}".format(session_id))
 		# aiomas.run(until=homeAgents[0].communicateBlockchain(bc_address))
 		aiomas.run()
 
@@ -47,8 +101,9 @@ def runContainer():
 	# Shutting donw the controller and thereby cleaning 
 	# all agents
 	try:
-		logger.info("Shutting down the home container...")
+		logger.info("Shutting down the home container...and cancelling the clock")
 		HC.shutdown()
+		t_clock_setter.cancel()
 		logger.info("Done.")
 	except Exception as e:
 		logger.info("Failed to shutdonw the home container")
--- trigger_home.py
+++ trigger_home.py
@@ -8,6 +8,14 @@ and ready to be kicked in!
 """
 import aiomas
 import sys, traceback
+from configure import Configure as CF
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Logging stuffs
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class TriggerAgent(aiomas.Agent):
 	def __init__(self, container):
@@ -15,20 +23,66 @@ class TriggerAgent(aiomas.Agent):
 
 	async def run(self, agent_addr):
 		home_agent = await self.container.connect(agent_addr)
-		print(home_agent)
+		logging.info(home_agent)
 		ret = await home_agent.trigger(agent_type="TRIGGER_AGENT")
-		print(ret)
+		logging.info(ret)
 
-# Possibly a database retrieve
-agent_lookup = {9019: 'tcp://localhost:5556/0',
-				7881: 'tcp://localhost:5556/1',}
 
-agent_id = sys.argv[1]
+def getAgentAddress(agent_id=None, session_id=None, db_engine=None):
+	"""
+	Retrieve the home agent's address given the
+	session id and agent id
+	"""
+	# Statement to retreive agent address
+	query = "SELECT `agent_address` FROM `tbl_agents` WHERE `agent_id`={} AND session_id='{}'".format(agent_id, session_id)
 
-agent_addr = agent_lookup[int(agent_id)]
-print(agent_addr)
+	# Dump the reseult into a dataframe
+	df = pd.read_sql(query, db_engine)
 
-c = aiomas.Container.create(('localhost', 5560))
+	if len(df) < 1:
+		return None
 
-trigger_agent = TriggerAgent(container=c)
-aiomas.run(until=trigger_agent.run(agent_addr))
+	return df['agent_address'][0]
+
+def main():
+	"""
+	"""
+	# Check the length of the command line arguments
+	if len(sys.argv) != 3:
+		logging.info("Wrong number of arguments.")
+		logging.info("Usage: python trigger_home.py <session_id> <home_id>")
+
+	# Session and agent(home) id
+	session_id = sys.argv[1]
+	agent_id = int(sys.argv[2])
+
+	# Create DB engine
+	# db_engine = create_engine("mysql+pymysql://{}@{}/{}".format(CF.DB_USER, CF.DB_HOST, CF.DB_NAME))
+	db_engine = CF.get_db_engine()
+
+	agent_addr = getAgentAddress(agent_id=agent_id, session_id=session_id, db_engine=db_engine)
+	
+	if agent_addr is None:
+		logging.info("Agent address couldn't be retreived.")
+		return
+
+	logging.info(agent_addr)
+
+	try:
+		c = aiomas.Container.create(('localhost', 5560))
+
+		trigger_agent = TriggerAgent(container=c)
+		aiomas.run(until=trigger_agent.run(agent_addr))	
+
+	except Exception as e:
+		logger.info("Failed to open/create container or run the triggering agent(s)")
+		traceback.print_exc(file=sys.stdout)
+	finally:
+		# Shutting down the container
+		logger.info("Shutting down the triggering agents container.")
+		c.shutdown()
+
+
+if __name__ == '__main__':
+	main()
+
