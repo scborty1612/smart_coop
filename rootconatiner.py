10d44e7770125533e89464d99c7e79267a073ed1
Shantanu Chakraborty <scborty@gmail.com>
2017-03-09 11:56:29 +0100

    Blockchain agent is registered into database

--- rootconatiner.py
+++ rootconatiner.py
@@ -17,6 +17,26 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+def recordAgent(agent_addr, agent_type, db_engine):
+	"""
+	Store the agent in DB
+	"""
+
+	# For now, just use the raw query
+	query = "INSERT INTO `tbl_agents` (`session_id`, `agent_id`, `agent_address`, "\
+			"`agent_type`, `status`, `insertion_datetime`) VALUES ('rootcontainersession', '-1', "\
+			"'{}', '{}', 'active', CURRENT_TIMESTAMP)".format(agent_addr, agent_type)
+
+	# Execute the query
+	try:
+		db_engine.execute(query)
+	except Exception as e:
+		logger.info("Query failed to execute!")
+		traceback.print_exc(file=sys.stdout)
+		return False
+
+	return True	
+
 def runContainer():	
 	# Creating container contains the home agents
 	# and prediction agents
@@ -24,6 +44,7 @@ def runContainer():
 
 	# Create the DB engine
 	# db_engine = create_engine("mysql+pymysql://{}@{}/{}".format(CF.DB_USER, CF.DB_HOST, CF.DB_NAME))
+	db_engine = CF.get_db_engine()
 
 	# Initiate the blockchain agent
 	blockChainAgent = BlockchainAgent(container=RC, )
@@ -31,12 +52,17 @@ def runContainer():
 	# Dump the blockchain agent address
 	logger.info("Blcokchain agent initiated at {}".format(blockChainAgent.addr))
 
+	# Record this agent to DB
+	status = recordAgent(agent_addr=blockChainAgent.addr, agent_type='blockchain', db_engine=db_engine)
+
+
 	# Run the event loop
 	try:
 		logger.info("Running the event loop. The blockchain agent is open to be connected!")
 		aiomas.run()
 	except KeyboardInterrupt:
-		logging.info("Keyboard Interrupted")		
+		logging.info("Keyboard Interrupted")
+
 	except Exception as e:
 		traceback.print_exc(file=sys.stdout)
 	# Shutting donw the controller and thereby cleaning 
